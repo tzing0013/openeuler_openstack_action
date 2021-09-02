cd4331a277868882d0c106a5a8509775139d3ba8
wangxiyuan <wangxiyuan1007@gmail.com>
2021-09-02 11:53:07 +0800

    Update package_status.py

--- package_status.py
+++ package_status.py
@@ -19,7 +19,7 @@ BRANCHS = [
 OBS_PACKAGE_BUILD_RESULT_URL = 'https://build.openeuler.org/build/%(branch)s/_result'
 GITEE_ISSUE_LIST_URL = 'https://gitee.com/api/v5/repos/openeuler/openstack/issues?state=open&labels=kind/obs-failed&sort=created&direction=desc&page=1&per_page=20'
 GITEE_ISSUE_CREATE_URL = 'https://gitee.com/api/v5/repos/openeuler/issues'
-GITEE_ISSUE_UPDATE_URL = 'https://gitee.com/api/v5/repos/{owner}/issues/%s'
+GITEE_ISSUE_UPDATE_URL = 'https://gitee.com/api/v5/repos/openeuler/issues/%s'
 SIG_PROJECT_URL = 'https://gitee.com/openeuler/community/raw/master/sig/sig-openstack/sig-info.yaml'
 OBS_USER_NAME = os.environ.get('OBS_USER_NAME')
 OBS_USER_PASSWORD = os.environ.get('OBS_USER_PASSWORD')
@@ -75,23 +75,25 @@ def update_issue(issue_number, result_str):
     }
     body = {
         "access_token": GITEE_USER_TOKEN,
-        "body":result_str,
+        "repo": "openstack",
+        "body": result_str,
     }
-    requests.patch(GITEE_ISSUE_UPDATE_URL % issue_number, headers=headers, params=body)
+    response = requests.patch(GITEE_ISSUE_UPDATE_URL % issue_number, headers=headers, params=body)
+    if response.status_code != 200:
+        raise Exception("Failed update gitee issue")
 
-
-def creat_issue(result_str):
+def create_issue(result_str):
     headers = {
         'Content-Type': 'application/json;charset=UTF-8',
     }
     body = {
         "access_token": GITEE_USER_TOKEN,
-        "repo":"openstack",
-        "title":"[CI] OBS Build Failed",
-        "body":result_str,
-        "labels":"kind/obs-failed",
-        "assignee":"huangtianhua",
-        "collaborators":"xiyuanwang"
+        "repo": "openstack",
+        "title": "[CI] OBS Build Failed",
+        "body": result_str,
+        "labels": "kind/obs-failed",
+        "assignee": "huangtianhua",
+        "collaborators": "xiyuanwang"
     }
     response = requests.post(GITEE_ISSUE_CREATE_URL, headers=headers, params=body)
     if response.status_code != 201:
